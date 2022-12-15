48c5e756045313836b8f9aa3f6b6cccb510ff594
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-12-15 18:49:19 -0500

    react app created

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -3,4 +3,25 @@ class AttemptsController < ApplicationController
         attempts = Attempt.all
         render json: attempts
     end
+
+    def create
+        attempt = Attempt.create(attempt_params)
+        if attempt
+            quiz = Quiz.find_by(id: attempt[:quiz_id])
+            # attempt.score = score_quiz(attempt, quiz)
+            render json: attempt, status: :created
+        else
+            render json: {errors: attempt.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def attempt_params
+        params.permit(:user_id, :quiz_id, :answers => [])
+    end
+
+    def score_quiz
+
+    end
 end
--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,4 +3,24 @@ class QuizzesController < ApplicationController
         quizzes = Quiz.all
         render json: quizzes
     end
+
+    def show
+        quiz = Quiz.find_by(id: params[:id])
+        render json: quiz
+    end
+
+    def create
+        quiz = Quiz.create(quiz_params)
+        if quiz.valid?
+            render json: quiz, status: :created
+        else
+            render json: {errors: quiz.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def quiz_params
+        params.permit(:user_id, :title, :questions => [], :answers => [])
+    end
 end
