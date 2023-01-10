1b5c65fe7faec5e7072ecbe4d3dbcf70ebe52ce5
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2023-01-10 17:20:53 -0500

    user can see average score of a quiz

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,4 +1,6 @@
 class Attempt < ApplicationRecord
     belongs_to :user 
     belongs_to :quiz
+
+
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,19 @@ class Quiz < ApplicationRecord
     belongs_to :user
     has_many :attempts
     has_many :users, through: :attempts
+
+
+    def avg_score
+        scores = self.attempts.map{ |attempt| 
+            if attempt.score != nil
+                attempt.score
+            else
+            end
+        }
+        if scores.compact.length > 0
+            scores.compact.sum / scores.compact.length
+        else
+            nil
+        end
+    end
 end
